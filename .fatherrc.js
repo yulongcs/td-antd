e8b72ef5945e528073c242c54a6c1a33a6e620d8
chenlong <long.chen@abssqr.com>
2020-02-17 21:43:36 +0800

    add doc-base

--- .fatherrc.js
+++ .fatherrc.js
@@ -1,10 +1,10 @@
 export default {
-  // cssModules: true, // 默认是 .module.css 走 css modules，.css 不走 css modules。配置 cssModules 为 true 后，全部 css 文件都走 css modules。（less 文件同理）
-  // extractCSS: true,
   esm: 'babel',
   cjs: 'babel',
-  // umd: false,
   extraBabelPlugins: [
     ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }],
   ],
+  doc: {
+    base: '/td-antd/',
+  },
 }
