eea8dc25dd684712349306e16fa57e598ccfb278
andy <andy@ryinfotech.com>
2020-04-14 14:02:59 +0800

    tools.downloadBlob支持从headers中获取文件名

--- src/tools/downloadBlob.js
+++ src/tools/downloadBlob.js
@@ -15,18 +15,17 @@ function downloadBlob(params) {
 
     return fetch(url, newOptions)
       .then(res => res.blob().then(blob => {
+        const name = filename || res.headers.get('Content-Disposition'); // 获取 headers 中的文件名
         // for IE
         if (window.navigator && window.navigator.msSaveOrOpenBlob) {
-          window.navigator.msSaveOrOpenBlob(blob, filename);
+          window.navigator.msSaveOrOpenBlob(blob, name);
         } else {
           const a = document.createElement('a');
           document.body.appendChild(a);
           // 获取 blob 本地文件连接 (blob 为纯二进制对象，不能够直接保存到磁盘上)
           const blobUrl = window.URL.createObjectURL(blob);
-          // 获取 headers 中的文件名
-          // const filename = res.headers.get('Content-Disposition');
           a.href = blobUrl;
-          a.download = filename;
+          a.download = name;
           a.click();
           window.URL.revokeObjectURL(blobUrl);
           a.remove();
