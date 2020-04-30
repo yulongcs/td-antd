e21aef9fe88e38e58a91d22d8a7c25744c4a4d04
andy <andy@ryinfotech.com>
2020-04-30 10:55:07 +0800

    修复

--- src/tools/toThousands.js
+++ src/tools/toThousands.js
@@ -1,11 +1,11 @@
-function toThousands(v) {
+function toThousands(v = 0) {
   if (typeof +v === 'number' && !isNaN(v) ) {
     return `${v}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
   } else {
     try {
-      return v.replace(/\$\s?|(,*)/g, '');
+      return +v.replace(/\$\s?|(,*)/g, '');
     } catch (e) {
-      throw new TypeError('参数不正确');
+      throw new TypeError(e);
     }
   }
 }
