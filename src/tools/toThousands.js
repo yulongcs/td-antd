68a0958e068085ef5f839c1c1082046476517054
andy <andy@ryinfotech.com>
2020-05-11 18:36:06 +0800

    feat: update toThousands

--- src/tools/toThousands.js
+++ src/tools/toThousands.js
@@ -1,6 +1,12 @@
-function toThousands(v = 0) {
+function toThousands(v = 0, decimal) {
   if (typeof +v === 'number' && !isNaN(v) ) {
-    return `${v}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    const str = `${v}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    if (decimal) {
+      const arr = str.split('.');
+      arr[1] = (`.${arr[1] || ''}`).padEnd(decimal + 1, 0);
+      return arr.join('');
+    }
+    return str;
   } else {
     try {
       return +v.replace(/\$\s?|(,*)/g, '');
