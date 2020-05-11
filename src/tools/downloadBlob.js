4fff0f8b1104429853a434cc4d740af2c2d50004
andy <andy@ryinfotech.com>
2020-05-11 11:42:29 +0800

    feat: downloadBlob重构

--- src/tools/downloadBlob.js
+++ src/tools/downloadBlob.js
@@ -1,57 +1,49 @@
 import toast from '../toast';
 
 function downloadBlob(params) {
-  try {
-    const { url, filename, body } = params;
-    const newOptions = { credentials: 'include', body };
+  const { url, filename, body } = params;
+  const newOptions = { credentials: 'include', body };
 
-    if (body) {
-      newOptions.headers = {
-        Accept: 'application/json',
-        'Content-Type': 'application/json; charset=utf-8',
-        ...newOptions.headers,
-      };
-      newOptions.method = 'POST';
-      newOptions.body = JSON.stringify(newOptions.body);
-    }
-
-    return fetch(url, newOptions)
-      .then(res => res.json().then(r => {
-        if (r.success) {
-          return res;
-        }
-        const error = new Error(r.errorMessage);
-        error.response = res;
-
-        throw error;
-      }))
-      .then(res => res.blob().then(blob => {
-        const name = filename || res.headers.get('Content-Disposition'); // 获取 headers 中的文件名
-        // for IE
-        if (window.navigator && window.navigator.msSaveOrOpenBlob) {
-          window.navigator.msSaveOrOpenBlob(blob, name);
-        } else {
-          const a = document.createElement('a');
-          document.body.appendChild(a);
-          // 获取 blob 本地文件连接 (blob 为纯二进制对象，不能够直接保存到磁盘上)
-          const blobUrl = window.URL.createObjectURL(blob);
-          a.href = blobUrl;
-          a.download = name;
-          a.click();
-          window.URL.revokeObjectURL(blobUrl);
-          a.remove();
-        }
-      }))
-      .catch((error) => {
-        if ('stack' in error && 'message' in error) {
-          toast({ type: 'error', text: error.message });
-        }
-
-        return error;
-      });
-  } catch (e) {
-    throw new TypeError(e);
+  if (body) {
+    newOptions.headers = {
+      Accept: 'application/json',
+      'Content-Type': 'application/json; charset=utf-8',
+      ...newOptions.headers,
+    };
+    newOptions.method = 'POST';
+    newOptions.body = JSON.stringify(newOptions.body);
   }
+
+  return fetch(url, newOptions)
+    .then(response => response.blob().then(blob => {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const value = e.target.result;
+        try {
+          const json = JSON.parse(value);
+          toast({ type: 'error', text: json.errorMessage });
+        }
+        catch(e) {
+          const name = filename || response.headers.get('Content-Disposition');
+          // for IE
+          if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+            window.navigator.msSaveOrOpenBlob(blob, name);
+          } else {
+            const a = document.createElement('a');
+            document.body.appendChild(a);
+            // 获取 blob 本地文件连接 (blob 为纯二进制对象，不能够直接保存到磁盘上)
+            const blobUrl = window.URL.createObjectURL(blob);
+            // 获取 headers 中的文件名
+            a.href = blobUrl;
+            a.download = name;
+            a.click();
+            window.URL.revokeObjectURL(blobUrl);
+            a.remove();
+          }
+        }
+      };
+      reader.readAsText(blob)
+    }))
 }
 
 export default downloadBlob;
