c272cc30b2189ae88f75b4155ddc921548515259
andy <andy@ryinfotech.com>
2020-06-01 11:44:12 +0800

    pagination新增新特性

--- src/pagination/index.js
+++ src/pagination/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import localeLanguage from './locale';
 
-export default (data, props = {}) => ({
-  hideOnSinglePage: true,
-  current: data.pageNum || data.currPageNo || 1,
-  pageSize: data.pageSize || data.limit || 20,
-  total: data.totalCnt || data.total || 1,
-  showTotal: (total) => <span>共查询到 <span style={{ color: '#f0882d' }}>{total}</span> 条数据</span>,
-  ...props,
-});
+export default ({ data, locale = 'en', ...rest }) => {
+  const language = localeLanguage[locale];
+
+  return {
+    hideOnSinglePage: true,
+    current: data.pageNum || data.currPageNo || 1,
+    pageSize: data.pageSize || data.limit || 20,
+    total: data.totalCnt || data.total || 1,
+    showTotal: total => <span dangerouslySetInnerHTML={{__html: language.totalTip.replace('$1', `<span style="color:#f0882d">${total}</span>`)}} />,
+    ...rest,
+  }
+};
---
+++ src/pagination/locale.js
@@ -0,0 +1,10 @@
+export default {
+  // 英文配置
+  en: {
+    totalTip: '$1 pieces of data found',
+  },
+  // 中文配置
+  zh: {
+    totalTip: '共查询到 $1 条数据',
+  },
+};
