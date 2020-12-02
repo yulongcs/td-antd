25632ac323e655d1f1f5b2de686ed989a99e6a0f
andy <andy@ryinfotech.com>
2020-12-02 18:22:59 +0800

    add delay.test.js

--- test/tools/delay.test.js
+++ test/tools/delay.test.js
@@ -2,14 +2,43 @@ import delay from '../../src/tools/delay';
 
 describe('delay', () => {
   // 测试异常抛出
-  test('compiling android goes as expected', () => {
+  it('compiling android goes as expected', () => {
     expect(delay).toThrow('Warning: [delay] Expected a function');
   });
 
-  // 测试回调函数
-  test('test callback', () => {
+  // 测试回调函数的参数
+  it('test the parmas of the callback', (done) => {
     delay((res) => {
-      expect(res).toBe('later2');
+      expect(res).toBe('later');
+      done();
     }, 3, 'later');
-  })
+  });
+
+  // 测试回调函数执行
+  it('test callback', (done) => {
+    let number = '0';
+    delay(() => {
+      number = '1';
+    }, 32);
+
+    setTimeout(() => {
+      expect(number).toBe('1');
+      done();
+    }, 64);
+  });
+
+  // 测试延迟函数被取消
+  it('should be cancelable', (done) => {
+    let number = '0',
+        timerId = delay(() => {
+          number = '1';
+        }, 32);
+
+    clearTimeout(timerId);
+
+    setTimeout(() => {
+      expect(number).toBe('0');
+      done();
+    }, 64);
+  });
 });
--- test/tools/toThousands.test.js
+++ test/tools/toThousands.test.js
@@ -9,12 +9,12 @@ describe('toThousands', () => {
   });
 
   // 入参是数字和小数点位数
-  test('test params is a number and params2 is 2', () => {
+  it('test params is a number and params2 is 2', () => {
     expect(toThousands(123456, 2)).toBe('123,456.00');
   });
 
   // 入参不是字符串数字
-  test('test params is not a number/string', () => {
+  it('test params is not a number/string', () => {
     const num = toThousands({});
 
     expect(errorSpy.mock.calls[0][0]).toMatch('Warning: [toThousands] Parameter type error');
@@ -22,22 +22,22 @@ describe('toThousands', () => {
   });
 
   // 入参是字符串
-  test('test params is a string', () => {
+  it('test params is a string', () => {
     expect(toThousands('12345.1')).toBe('12,345.1');
   });
 
   // 入参是字符串和小数点位数
-  test('test params is a string and params2 is 2', () => {
+  it('test params is a string and params2 is 2', () => {
     expect(toThousands('12345.1', 2)).toBe('12,345.10');
   });
 
   // 入参是千位符分割的字符串
-  test('test params is a currency string', () => {
+  it('test params is a currency string', () => {
     expect(toThousands('123,456')).toBe(123456);
   });
 
   // 入参是千位符分割的字符串
-  test('test currency', () => {
+  it('test currency', () => {
     expect(toThousands.currency(12345)).toBe('￥12,345.00');
   });
 });
