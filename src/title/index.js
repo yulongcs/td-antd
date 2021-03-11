12d80c5a47fb6c8eed812983cd4504567d015101
andy <andy@ryinfotech.com>
2021-03-11 11:11:25 +0800

    title add extra

--- src/title/index.js
+++ src/title/index.js
@@ -4,21 +4,24 @@ import { Tooltip } from 'antd';
 import { QuestionCircleOutlined } from '@ant-design/icons';
 import './index.less';
 
-export default ({ required = false, colon = true, name = '', tip, className, ...rest }) => {
+export default ({ required = false, colon = true, name = '', tip, extra, className, ...rest }) => {
   return (
-    <div
-      className={cx('td-title-wrap', className, {
-        'td-title-required': required,
-        'td-title-colon': colon,
-      })}
-      {...rest}
-    >
-      {name}
-      {tip && (
-        <Tooltip title={tip}>
-          <QuestionCircleOutlined className="td-title-tip" />
-        </Tooltip>
-      )}
+    <div className="td-title-wrap">
+      <div
+        className={cx('td-title-main', className, {
+          'td-title-required': required,
+          'td-title-colon': colon,
+        })}
+        {...rest}
+      >
+        {name}
+        {tip && (
+          <Tooltip title={tip}>
+            <QuestionCircleOutlined className="td-title-tip" />
+          </Tooltip>
+        )}
+      </div>
+      {extra}
     </div>
   );
 }
