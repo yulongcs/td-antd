9ba77ca13e8cedfac5e9c90f2b41d44262dbf95c
andy <andy@ryinfotech.com>
2021-04-30 16:16:59 +0800

    desc-list添加测试用例

--- test/setup.js
+++ test/setup.js
@@ -1,6 +1,15 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+// 自定义一个node环境的媒体查询对象
+window.matchMedia = window.matchMedia || function() {
+  return {
+    matches : false,
+    addListener : function() {},
+    removeListener: function() {},
+  };
+};
+
 Enzyme.configure({
   adapter: new Adapter(),
 });
