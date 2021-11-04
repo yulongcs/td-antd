e8bd248dbe1cb4347f34bd1068e6635600b28052
long-zhuge <158362530@qq.com>
2021-11-04 10:47:12 +0800

    修复clipboard在safari下失效的问题

--- src/tools/clipboard.js
+++ src/tools/clipboard.js
@@ -1,18 +1,30 @@
 function clipboard(text) {
-  const copyEvent = (e) => {
-    e.clipboardData.setData('text/plain', text);
-    e.preventDefault();
-  };
+  if (document.execCommand('copy')) {
+    const copyEvent = (e) => {
+      e.clipboardData.setData('text/plain', text);
+      e.preventDefault();
+    };
 
-  document.addEventListener('copy', copyEvent);
+    document.addEventListener('copy', copyEvent);
 
-  try {
+    try {
+      document.execCommand('copy');
+      document.removeEventListener('copy', copyEvent);
+
+      return Promise.resolve();
+    } catch (err) {
+      throw new TypeError('Warning: [clipboard] Unable to copy');
+    }
+  } else {
+    const input = document.createElement('textarea');
+    input.value = text;
+    document.body.appendChild(input);
+    input.setSelectionRange(-1, 9999);
+    input.select();
+
     document.execCommand('copy');
-    document.removeEventListener('copy', copyEvent);
-
+    document.body.removeChild(input);
     return Promise.resolve();
-  } catch (err) {
-    throw new TypeError('Warning: [clipboard] Unable to copy');
   }
 }
 
