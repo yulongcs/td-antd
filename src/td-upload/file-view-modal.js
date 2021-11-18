0b3bf55df0050816dfcf209d8c402183a1b5fc84
long-zhuge <158362530@qq.com>
2021-11-18 10:49:31 +0800

    td-upload新增属性

--- src/td-upload/file-view-modal.js
+++ src/td-upload/file-view-modal.js
@@ -8,7 +8,9 @@ export default forwardRef((props, ref) => {
   const [loading, setLoading] = useState(true);
   const [visible, setVisible] = useState(false);
   const [tip, setTip] = useState('');
-  const { url, showDownLoad, beforeDownload } = props;
+  const { fileObject, showDownLoad, beforeDownload, children, ...rest } = props;
+
+  const { name, url } = fileObject;
 
   const show = () => {
     setVisible(true);
@@ -28,8 +30,10 @@ export default forwardRef((props, ref) => {
   }));
 
   const download = () => {
+    if (!url) return;
+
     const a = document.createElement('a'); // 创建a标签
-    a.setAttribute('download', ''); // download属性
+    a.setAttribute('download', name); // download属性
     a.setAttribute('href', url); // href链接
     a.setAttribute('target', '_blank');
     a.click(); // 自执行点击事件
@@ -55,16 +59,19 @@ export default forwardRef((props, ref) => {
       visible={visible}
       onCancel={() => { setVisible(false) }}
       title={showDownLoad ? <LinkBtn onClick={onClick}>下载</LinkBtn>: '预览'}
+      {...rest}
     >
-      <Spin spinning={loading} tip="图片加载中...">
-        <img
-          alt={tip}
-          src={url}
-          onLoad={onLoad}
-          onError={onError}
-          className="td-img-modal-img"
-        />
-      </Spin>
+      {children ? children(fileObject) : (
+        <Spin spinning={loading} tip="图片加载中...">
+          <img
+            alt={tip}
+            src={url}
+            onLoad={onLoad}
+            onError={onError}
+            className="td-img-modal-img"
+          />
+        </Spin>
+      )}
     </Modal>
   );
 })
